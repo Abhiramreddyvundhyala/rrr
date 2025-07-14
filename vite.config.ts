bf7e34bfd8c6ddc8a8573cc4c69049e6a315d65d
Abhi <abhi@sams.com>
2025-07-14 14:19:01 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rrr/', // 👈 necessary for GitHub Pages under repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
