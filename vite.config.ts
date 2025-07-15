0a2a1de253e71dc5a87e789cd3717f59b7d65ae5
Abhiramreddy vundhyala <abhiabhiramreddy32@gmail.com>
2025-07-15 22:03:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rrr/', // 👈 very important!
+  base: '/', // ✅ Correct if you're using a custom domain
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 })
